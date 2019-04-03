8fc9fb3609cb05efc294758b10158961c55067b3
akash2237778 <akash2237778@gmail.com>
2019-04-03 22:30:38 +0530

    modified main to add file to stagged folder

--- skeleton.cpp
+++ skeleton.cpp
@@ -7,24 +7,36 @@ using namespace std;
 
 
 class VCSadd{};
+
 class VCScommit{};
 
-int main(){ CreatingFil obj1;    //obj1 to create file
+int main(){
+     CreatingFil obj1;    //obj1 to create file
      VCSInitialize obj2;  //obj2 to initialize directory VCS
      fileCopyMaker obj3;   //obj3 to copy files
-
+     string fileName;
 
     obj2.VCSinitialize();
+   fileName = obj1.CreateFil();
 
-    string fileName;
+    cout<<"\n Want to add file to VCS press 1 >>>>";    //to add file to stagged area
+    int options;
+    cin>>options;                                       //getting user choice
+    if(options == 1){
+        string copyName = fileName + "1" +  ".txt";
+        fileName = fileName + ".txt";
 
-    fileName = obj1.CreateFil() + ".txt";
+        obj3.CopyMaker(fileName,copyName , "stagedFiles" );  //sending redefined arguments
 
-      obj3.CopyMaker(fileName);
+    }
 
 
+//cout<<" \n Enter the name of file to be added :";
 
+  //      cin>>fileName;
     cout<<"\n"<<fileName;
+
+
 
 return 0;
 }
