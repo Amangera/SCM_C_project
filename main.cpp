98bca837f4ee757f0711ecca05e744c200cc84c4
AkanshA0 <akkansha01@gmail.com>
2019-04-04 23:17:17 +0530

    made menu driven

--- main.cpp
+++ main.cpp
@@ -11,23 +11,35 @@ class VCSadd{};
 class VCScommit{};
 
 int main(){
-     CreatingFil obj1;    //obj1 to create file
+     int choice;
+     cout<<"Press 0 to create new file and 1 to add file to VCS >>>>";
+     cin>>choice;
+     
+    
      VCSInitialize obj2;  //obj2 to initialize directory VCS
      fileCopyMaker obj3;   //obj3 to copy files
      string fileName;
 
     obj2.VCSinitialize();
-   fileName = obj1.CreateFil();
-
-    cout<<"\n Want to add file to VCS press 1 >>>>";    //to add file to stagged area
-    int options;
-    cin>>options;                                       //getting user choice
-    if(options == 1){
+    switch(choice)
+    {
+       case 0:
+	       {
+       CreatingFil obj1;    //obj1 to create file
+       fileName = obj1.CreateFil();
+       break;
+	       }
+       case 1:
+	       {
         string copyName = fileName + "1" +  ".txt";
         fileName = fileName + ".txt";
 
         obj3.CopyMaker(fileName,copyName , "stagedFiles" );  //sending redefined arguments
-
+       break;
+    }
+       default:
+	       cout<<"Enter valid operation >>>>";
+	       break;
     }
 
 
